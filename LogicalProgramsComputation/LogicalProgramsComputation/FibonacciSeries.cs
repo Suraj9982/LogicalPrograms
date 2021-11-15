fd0bb50c88e99ebd0ca3edb3244d05e13078d794
suraj9982 <surajsk9982@gmail.com>
2021-11-15 16:53:50 +0530

    [suraj] add : this program prints fibonacci series

---
+++ LogicalProgramsComputation/LogicalProgramsComputation/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsComputation
+{
+    class FibonacciSeries
+    {
+        public void FindFibonacci(int length)
+        {
+            int num1 = 0, num2 = 1, num3, i;
+            Console.WriteLine("{0}\n{1}", num1, num2);
+            for (i = 2; i < length; i++)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine("{0}", num3);
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
---
+++ LogicalProgramsComputation/LogicalProgramsComputation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LogicalProgramsComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE ONE OPTION\n1.Fibonacci Series\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        Console.WriteLine("enter the length for fibonacci series");
+                        int length = Convert.ToInt32(Console.ReadLine());                       
+                        series.FindFibonacci(length);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice should be between 1 to 2");
+                        break;
+                       
+                }
+            }
+        }
+    }
+}
